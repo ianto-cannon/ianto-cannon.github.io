3a3a2a4d04b6cdb3f127593684cceb766d248006
ianto-cannon <ianto.cannon@gmail.com>
2026-05-21 12:38:46 +0200

    allow the Bonusmember comment

--- guestbook.js
+++ guestbook.js
@@ -8,7 +8,7 @@ async function load() {
     container.innerHTML = "";
     data.forEach(m => {
      
-      if (m.includes('Bonusmember')) return; 
+      //if (m.includes('Bonusmember')) return; 
 
       const div = document.createElement("div");
       div.className = "message";
